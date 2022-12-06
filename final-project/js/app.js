dee0d87ab24bc653c456b553d192010bf7a96eb0
salonibg <99049157+salonibg@users.noreply.github.com>
2022-12-06 13:30:27 -0500

    update

--- final-project/js/app.js
+++ final-project/js/app.js
@@ -1,4 +1,35 @@
-//populate cover images on home (DOM elements)
+//TIMELINE
+var items = document.querySelectorAll("li");
 
+function isItemInView(item){
+  var rect = item.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 
+function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      var featured = document.getElementById("featured");
+      var toggle = document.getElementById("button-1");
+      console.log(toggle.knobs)
+      if (isItemInView(items[i]) ) {
+        //&& featured.style.display == 'block'
+        items[i].classList.add("show");
+        //featured.style.display = 'none';
+        //toggle.innerHTML = 'Featured';
+      } else {
+        //featured.style.display = 'block';
+        //toggle.innerHTML = 'All';
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
 
